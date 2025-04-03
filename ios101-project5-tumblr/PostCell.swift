b310a1c023a85ae67cca1169322b115fb0f85b02
nishan <nishanlama03@gmail.com>
2025-04-02 23:45:10 -0700

    image view updated

---
+++ ios101-project5-tumblr/PostCell.swift
@@ -0,0 +1,14 @@
+//
+//  PostCell.swift
+//  ios101-project5-tumblr
+//
+//  Created by Nishan Lama on 4/2/25.
+//
+import UIKit
+
+class PostCell: UITableViewCell {
+    
+    @IBOutlet weak var summaryLabel: UILabel!
+    @IBOutlet weak var postImageView: UIImageView!
+}
+
--- ios101-project5-tumblr/ViewController.swift
+++ ios101-project5-tumblr/ViewController.swift
@@ -2,21 +2,24 @@
 //  ViewController.swift
 //  ios101-project5-tumbler
 //
-
 import UIKit
 import Nuke
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+    
+    @IBOutlet weak var tableView: UITableView!
+    var posts: [Post] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        tableView.dataSource = self
         fetchPosts()
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 150
+
     }
-
-
 
     func fetchPosts() {
         let url = URL(string: "https://api.tumblr.com/v2/blog/humansofnewyork/posts/photo?api_key=1zT8CiXGXFcQDyMFG7RtcfGLwTdDjFUJnZzKJaWTmgyK4lKGYk")!
@@ -38,16 +41,9 @@ class ViewController: UIViewController {
 
             do {
                 let blog = try JSONDecoder().decode(Blog.self, from: data)
-
                 DispatchQueue.main.async { [weak self] in
-
-                    let posts = blog.response.posts
-
-
-                    print("✅ We got \(posts.count) posts!")
-                    for post in posts {
-                        print("🍏 Summary: \(post.summary)")
-                    }
+                    self?.posts = blog.response.posts
+                    self?.tableView.reloadData()
                 }
 
             } catch {
@@ -55,5 +51,23 @@ class ViewController: UIViewController {
             }
         }
         session.resume()
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let post = posts[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell", for: indexPath) as! PostCell
+
+        cell.summaryLabel.text = post.summary
+
+        if let photo = post.photos.first {
+            let url = photo.originalSize.url
+            Nuke.loadImage(with: url, into: cell.postImageView)
+        }
+
+        return cell
     }
 }
